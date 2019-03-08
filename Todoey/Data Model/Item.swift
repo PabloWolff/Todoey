6795bbe93c3ede4bf1a6d0116a0170c6d301cd09
cb071564 <pwolff@freshexpress.com>
2019-03-08 10:43:01 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by  on 3/6/19.
+//  Copyright © 2019 Pablo Wolff. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
